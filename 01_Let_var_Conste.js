74bf43b7220ac7866d7bd6973894c3d6a659a9c4
Sameer Khan <sameerkhanbpk8@gmail.com>
2024-03-15 23:15:40 +0500

    JavaScript new Course

---
+++ 01_Let_var_Conste.js
@@ -0,0 +1,8 @@
+const CustomerId = 23234;
+let AccountEmail = "john.doe@example.com";
+var AcountPassword = "khan321";
+// AccountCity="swabi";
+// CustomerId = 2323
+AccountEmail =  "khan@4321.com";
+AcountPassword="poeswe4321";
+console.table([CustomerId,AccountEmail,AcountPassword])
---
+++ 02_dataType.js
@@ -0,0 +1,15 @@
+"use strict" // treat all code as a newer version
+
+// alert("hello World") we are using Node.js not web Browser
+
+let Name = "Sameer" //string
+let Age = 23       //number
+let isThere = true //boolean
+
+// string => 2 to power 53
+// bigint
+// Boolean => true/false
+// null => represents nothing, like void in C
+// undefined => variable has been declared but no value assigned yet
+// object => collection of properties
+console.log(typeof isThere)
